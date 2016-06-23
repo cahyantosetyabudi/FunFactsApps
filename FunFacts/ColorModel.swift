ca963c213f21e01b409b6a9615a5ec674a3115e0
cahyantosetyabudi <executionzee@gmail.com>
2016-06-24 06:47:41 +0700

    Final

---
+++ FunFacts/ColorModel.swift
@@ -0,0 +1,9 @@
+//
+//  ColorModel.swift
+//  FunFacts
+//
+//  Created by Cahyanto Setya Budi on 6/24/16.
+//  Copyright © 2016 Cahyanto Setya Budi. All rights reserved.
+//
+
+import Foundation
---
+++ FunFacts/FactModel.swift
@@ -0,0 +1,9 @@
+//
+//  FactModel.swift
+//  FunFacts
+//
+//  Created by Cahyanto Setya Budi on 6/24/16.
+//  Copyright © 2016 Cahyanto Setya Budi. All rights reserved.
+//
+
+import Foundation
